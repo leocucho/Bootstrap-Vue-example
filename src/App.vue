7c1711f26c79a687705a4905ac1505bc4e4ec417
leo_cucho <leonardocucho@gmail.com>
2021-04-26 15:46:36 -0500

    other

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <NavBar/>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <Nuevo/>
+      <Galeria/>
+    
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import NavBar from './components/NavBar.vue'
+import Nuevo from './components/Nuevo.vue'
+import Galeria from './components/Galeria.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {    
+    NavBar,
+    Nuevo,
+    Galeria
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
+
+
---
+++ src/components/Nuevo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row"> 
+      <div class="col-6">
+          <b-table striped hover :items="items" :fields="fields"></b-table>
+      </div>
+      <div class="col-6">
+          <h1 class="text-center">Prueba</h1>
+
+          <div class="row">
+      <div class="col-6"><h2>texto de prueba</h2></div>
+      <div class="col-6"><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste expedita iure eveniet doloremque inventore aliquam nemo. Eum saepe dicta placeat quod dignissimos quo magni assumenda labore, ab eius eligendi vel.</p></div>
+          </div>
+      </div>
+  </div>
+  
+  
+</template>
+
+<script>
+  export default {
+      name: 'Nuevo',
+    data() {
+      return {
+        // Note `isActive` is left out and will not appear in the rendered table
+        fields: ['first_name', 'last_name', 'age'],
+        items: [
+          { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ]
+      }
+    }
+  }
+</script>
